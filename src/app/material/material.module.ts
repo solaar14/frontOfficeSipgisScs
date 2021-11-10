51f47f9c8867e0e2252be31058e383c0ec034010
solaar14 <eric.creahi@gmail.com>
2021-11-10 07:42:45 +0000

    ajouter ngx-Pagination à mes module devop

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatTableModule} from '@angular/material/table';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 const MaterialComponent = [
   MatButtonModule,
-  MatTableModule
+  MatTableModule,
+  PaginationModule
+
 ]
 
 
